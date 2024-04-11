04c1eb5fd21174fbf7ec2a85239a247bc4ca8759
edup3 <eduardopineros@hotmail.com>
2024-04-11 14:46:52 -0500

    inicio desarrollo

--- image_generator/views.py
+++ image_generator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
+def home(request):
+    return render(request,'vacante.html')
+def image_gen(request):
+    return render(request,'image_gen.html')
