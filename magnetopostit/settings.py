b629f2ea2f889ebe96024c97342232d8f4694260
edup3 <eduardopineros@hotmail.com>
2024-04-16 15:48:53 -0500

    Evitar problemas de database

--- magnetopostit/settings.py
+++ magnetopostit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*d8#sa6p$!iwgne&-9fko7r+9l*_pox8=kygi1s*8io@09kcd*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
